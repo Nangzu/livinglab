62bd84415f4f2b4fe6d9f7ed58a7dd26ab3f4e18
khc312 <lgh980304@gmail.com>
2025-01-14 20:25:16 +0900

    유저 dto랑 entity 변수 통일화 -> 잘 받아짐

--- src/main/java/com/example/livinglab/Dto/UserDTO.java
+++ src/main/java/com/example/livinglab/Dto/UserDTO.java
@@ -9,24 +9,24 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDTO {
-    private Long userNum;  // user_num
+    private Long user_num;  // user_num
     private String userid;     // id
     private String phone;   // phone
     private String email;   // email
     private String address; // address
-    private String userName; // user_name
+    private String user_name; // user_name
 
     // Role 필드가 필요한 경우, RoleDTO를 사용하거나 Role 정보를 문자열로 반환할 수 있음
     private String role;    // role 정보가 필요하다면
 
     // User 엔티티를 UserDTO로 변환하는 생성자 추가
     public UserDTO(User user) {
-        this.userNum = user.getUser_num();
+        this.user_num = user.getUser_num();
         this.userid = user.getUserid();
         this.phone = user.getPhone();
         this.email = user.getEmail();
         this.address = user.getAddress();
-        this.userName = user.getUser_name();
+        this.user_name = user.getUser_name();
         this.role = user.getRole() != null ? user.getRole().getRoleName() : null;  // roleName을 Role 클래스에서 가져올 수 있음
     }
 }
